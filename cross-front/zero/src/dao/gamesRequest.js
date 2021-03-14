32c8ef1a3924ba041b5edfbb04831690e456a49e
pomahcku <rom2517pop@gmail.com>
2021-03-14 21:52:12 +0300

    Moved front to separate project

--- cross-front/zero/src/dao/gamesRequest.js
+++
@@ -1,7 +0,0 @@
-import request from "./request";
-
-export function getGames() {
-    return request.get('game')
-        .then(request => request.data)
-        .catch(err => console.log(err.message))
-}
